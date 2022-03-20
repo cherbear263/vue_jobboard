3888322cd75a144585283379feb9cf35f51a156d
msjones3 <cheryl@realsocial.com.au>
2022-03-20 18:16:13 +1000

    add prev forward and redirect

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,32 @@
     <router-link :to="{ name: 'About' }">About</router-link> |
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </nav>
+
   <router-view/>
+  <div class="inline-flex mt-16">
+    <button @click="back" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l text-sm">Prev</button>
+    <button @click="redirect" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 text-sm">Home</button>
+    <button @click="forward" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r text-sm">Next</button>
+  </div>
 </template>
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home'})
+
+    },
+    back() {
+      this.$router.go(-1)
+
+    },
+    forward() {
+      this.$router.go(1)
+
+    },
+  }
+}
+</script>
 
 <style>
 #app {
