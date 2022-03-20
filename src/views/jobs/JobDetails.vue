e067d47ce593611cb5f8044fc7b8134002a888e7
msjones3 <cheryl@realsocial.com.au>
2022-03-20 16:46:15 +1000

    add job details page placeholder

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,15 @@
+<template>
+<!-- job details page - from path /jobs/:id -->
+<!-- access the id of the route by using $route.params.id -->
+  <h1>Job Details Page</h1>
+  <p>The job ID is {{ id }}</p>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id
+    }
+  }
+}
+</script>
